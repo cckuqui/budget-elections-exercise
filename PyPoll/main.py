f3add36027213c61510ece64af58c39e856576a2
Cris B <cristina.bardan@gmail.com>
2020-02-07 23:55:50 -0600

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,39 +27,15 @@ with open(csvpath, newline="") as csvfile:
             candidates[votes] = 1
 
 print(candidates)
-print(total_votes)
+
+# Add value in percentage
 
 
-
-    
-# # Save values into months and profits (as an integer) lists
-#     for row in csvreader: 
-#         candidate = row[2]
-#         months.append(row[0])
-#         profits.append(int(row[1]))
-
-# # Calculate differences in profits and save info in monthly_change list
-#     for i in range(len(profits)-1):
-#         monthly_change.append(profits[i+1]-profits[i])
-
-#     # print(monthly_change)
-
-# # Get max and min values from monthly_change list and relate it to month list
-# max_monthly_change = max(monthly_change)
-# min_monthly_change = min(monthly_change)
-
-# max_change_month = monthly_change.index(max(monthly_change)) + 1
-# min_change_month = monthly_change.index(min(monthly_change)) + 1 
-
-# # Define variables to Print
-# lengh = len(profits)
-# total_profits = sum(profits)
-# ave_change = round(sum(monthly_change)/len(monthly_change),2)
-
-# # Print results
-# line1 = print("Financial Analysis")
-# line2 = print("----------------------------")
-# print("Total Months: " + str(lengh))
+# Print results
+print("Election Results")
+print("----------------------------")
+print("Total Votes: " + str(total_votes))
+print("----------------------------")
 # print("Total: $" + str(total_profits))
 # print("Average Change: " + str(ave_change))
 # print("Greatest Increase in Profits: " + months[max_change_month] + " ($" + str(max_monthly_change) + ")")
